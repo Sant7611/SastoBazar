b9cc830aa9ad32476ab18e8d733270623fb406c8
santosh bohara <108050563+Sant7611@users.noreply.github.com>
2024-08-26 01:53:35 +0000

    footer added

---
+++ kbc/5th_Semester/SastoBazar/client/src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+import Header from "./components/header/MyHeader";
+import Footer from "./components/footer/Footer";
+
+function App() {
+  return (
+    <div>
+    <Header />
+    <Footer />
+    </div>
+  );
+}
+
+export default App;
